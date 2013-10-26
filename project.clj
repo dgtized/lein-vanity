164b8c110c89f8e97ff6f0ca699a452aaf99552a
Charles Comstock <dgtized@gmail.com>
2013-10-25 22:50:44 -0500

    bump version to snapshot and upgrade to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-vanity "0.1.0"
+(defproject lein-vanity "0.2.0-SNAPSHOT"
   :description "Vanity clojure LOC metrics plugin for leiningen"
   :url "https://github.com/dgtized/lein-vanity"
   :license {:name "MIT"
             :url "https://raw.github.com/dgtized/lein-vanity/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :min-lein-version "2.0.0"
   :eval-in-leiningen true)
